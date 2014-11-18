6f3a5f7d5861b5850e59969e93996686bcc6d266
triphop35 <triphop7768@gmail.com>
2014-11-19 00:23:33 +0700

    12th : Insert book in db

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ var express = require('express');
 var mongojs = require('mongojs');
 var app = express();
 var db = mongojs('my_server', ['book']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 app.get('/api/book',function(req,res){
 	db.book.find({},function(err,books){
 	res.send(books);
 	})	
  })
+
+app.post('/api/book',function(req,res){
+	db.book.insert((req.body),function(err,books){
+		res.send(books);	
+	});
+});
 
 var server = app.listen(3000, function () {
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,16 @@
 angular.module("myApp", [])
 .controller('mainCtrl', function($scope,$http){
 	$scope.books = [];
+	$scope.bookInstance ={};
 	$http.get('/api/book').success(function(data){
 		$scope.books=data;
 	})
+	$scope.save = function(){
+		$http.post('/api/book',$scope.bookInstance).success(function(data){
+	    	console.log($scope.bookInstance);
+	    	$scope.books.push(data);
+	    	console.log(data);
+	    	$scope.bookInstance = {};
+	  	});
+	}
 })
